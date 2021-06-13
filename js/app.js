1980d39eb3c10680556acb02c3f2d03abebc451f
goncmodev <gon.cmo.dev@gmail.com>
2021-06-13 02:33:53 +0100

    First steps in project

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// variables
+const formulario = document.querySelector('#formulario');
+const listaTweets = document.querySelector('#lista-tweets');
+let tweets = [];
+
+// Eventos
+eventListeners();
+
+function eventListeners(){
+        formulario.addEventListener('submit', agregarTweet);
+
+}
+
+
+// Funciones
+function agregarTweet(e){
+        e.preventDefault();
+
+        console.log('agregando tweet...');
+}
